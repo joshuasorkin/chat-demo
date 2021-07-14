58fd2c0807259969a08e2d8a90b1134fc0ad462e
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-07-14 13:47:42 -0700

    added socket.io connection event

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000
+const io = require('socket.io')(server,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials:true
+    }
+});
+const path = require('path');
+const cors = require('cors');
+
+/*RouterLoader.loadRoutes() was failing to load testAPI when
+called from module, so added the loadRoute code here
+until that is fixed and we can refactor the function
+back to the module*/
 //const RouterLoader = require('./routerLoader');
 //const routerLoader = new RouterLoader(app);
+//routerLoader.loadRoutes();
 
+//initialize view engine
 app.set('view engine', 'pug')
+const path = require('path');
 
-//begin router initializer
+//set index page to static HTML file, using index.html
+app.use(express.static(path.join(__dirname + '/public')))
+
+
+//begin router initializer for REST API
 var route_directory = "routes";
 var filenames = fs.readdirSync(route_directory);
 var router;
@@ -23,13 +43,21 @@ filenames.forEach(filename=>{
         eval("app.use('/"+router+"',"+router+"Router);");
     }
     else{
-        console.log("adding default route");
-        eval("app.use('/',"+router+"Router);");
+        /*index is being served as index.html through the express.static() call
+        leaving this section in place for future development
+        if we want to dynamically render the index page*/
+
+        //console.log("adding default route");
+        //eval("app.use('/',"+router+"Router);");
     }
 })
 //end router initializer
 
-//routerLoader.loadRoutes();
+
+io.on('connection',socket=>{
+    console.log('Client connected')
+})
+
 
 server.listen(port, () => {
     console.log(`Server running on port: ${port}`);
