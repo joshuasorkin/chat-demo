011364f36745b9587d4fbb75e9c82f01dc622418
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-07-14 14:16:10 -0700

    added socket event to client UI

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
 const socket = io();
+
+//get objects corresponding to various page elements
+//from the DOM
+const chat = document.querySelector('.chat-form');
+const input = document.querySelector('.chat-input');
+
+//add event listener to submit button
+chat.addEventListener('submit',event =>{
+    //prevent page from reloading on form submission
+    event.preventDefault();
+    /*send an event called 'chat' to server-side socket
+    carrying the submitted chat message*/
+    socket.emit('chat',input.value);
+    //reset chat input field to blank once the message has been sent
+    input.value = '';
+});
+
