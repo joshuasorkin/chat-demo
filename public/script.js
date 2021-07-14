1cf252b734305cd47f9fa982d71fdbf20c0a1d39
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-07-14 14:37:38 -0700

    modified UI to display received messages in chat window instead of console.log

--- public/script.js
+++ public/script.js
@@ -4,7 +4,14 @@ const socket = io();
 //from the DOM
 const chat = document.querySelector('.chat-form');
 const input = document.querySelector('.chat-input');
+const chatWindow = document.querySelector('.chat-window');
 
+const renderMessage = message => {
+    const div = document.createElement('div')
+    div.classList.add('render-message');
+    div.innerText = message;
+    chatWindow.appendChild(div);
+}
 //add event listener to submit button
 chat.addEventListener('submit',event =>{
     //prevent page from reloading on form submission
@@ -18,5 +25,5 @@ chat.addEventListener('submit',event =>{
 
 //add event listener for receiving 'chat' message from server
 socket.on('chat',message=>{
-    console.log('From server: ',message);
+    renderMessage(message);
 })
