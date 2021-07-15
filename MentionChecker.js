37e874c2454dc59c6e3dedc607c4bb68f1f1b487
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-07-15 13:03:48 -0700

    testing case-sensitivity

--- MentionChecker.js
+++ MentionChecker.js
@@ -1,13 +1,13 @@
 class MentionChecker{
-    constructor(NameChecker){
-        this.NameChecker=NameChecker;
+    constructor(nameChecker){
+        this.nameChecker=nameChecker;
         this.mentionSigil="@";
         this.regex="/@[A-Za-z0-9]+/g"
     }
 
     formatMentions(message){
         return message.replace(/@[A-Za-z0-9]+/g,function(match){
-            if(this.NameChecker.getIDFromName(match)!==null){
+            if(this.nameChecker.getIDFromName(match)!==null){
                 return "<strong>"+match+"</strong>";
             }
             else{
