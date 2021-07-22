b2f061c77af76e5aaa9eaeb05a8d9d7a08822dba
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-07-22 10:46:32 -0700

    change firebaseConfig.initializeApp to firebase.initializeApp

--- public/auth.js
+++ public/auth.js
@@ -7,7 +7,7 @@
     messagingSenderId: "307425308969",
     appId: "1:307425308969:web:c46450f53bc49e8e8d8738"
 };
-firebaseConfig.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const txtEmail=document.getElementById('txtEmail');
 const txtPassword=document.getElementById('txtPassword');
